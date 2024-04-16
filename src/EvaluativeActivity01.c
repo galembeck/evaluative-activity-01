d038415fa007ea27f601cbc72665f0288a10731a
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-04-16 13:26:52 -0300

    refactor: update final project version

--- src/EvaluativeActivity01.c
+++ src/EvaluativeActivity01.c
@@ -1,13 +1,15 @@
-#include <stdio.h> // Including/importing the <stdio.h> (Standard Input Output) library
-#include <stdlib.h> // Including/importing the <stdlib.h> (Standard System) library
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <unistd.h> // Including/importing the <unistd.h> library - using "sleep() in code"
+#include <unistd.h>
 
-#include <locale.h> // Including/importing the <locale.h> (Language/Localization) library
+#include <string.h>
 
-#define length 25 // Defining the maximum length of the vector ("safe_bar") to 25 positions (0 - 24)
+#include <locale.h>
 
-void introMessage() { // Creating a "introMessage()" function to show the introduction message
+#define length 25
+
+void introMessage() {
     printf("\n Olá! Eu sou um cofre virtual...");
     printf("\n Eu represento uma simulação digital de um mecanismo de segurança.");
 
@@ -18,19 +20,12 @@ void introMessage() { // Creating a "introMessage()" function to show the introd
     printf("\n\n Aqui está o funcionamento: possuo uma barra de 25 posições, começando na posição 10, que pode ser movida até 4 vezes.");
     printf("\n Durante cada movimento, os números entre os novos limites estabelecidos serão contados.");
 
-    // printf("\n\n Essa é a barra:");
-    // printf(" | 1 | 5 | 7 | 8 | 2 | 2 | 7 | 8 | 2 | 5 | (7) | 8 | 2 | 1 | 5 | 2 | 7 | 8 | 2 | 1 | 1 | 2 | 7 | 8 | 2 |");
-
     printf("\n\n O número 7, na posição 10, marca o início dos movimentos.\n A partir deste ponto, você moverá os limites para revelar a combinação correta.");
 
     printf("\n\n Agora, com todas as instruções, podemos começar!\n\n");
 }
 
-// void safeBarShowcase() { // Creating a "safeBarShowcase()" to show the bar when necessary
-//    printf("\n | 1 | 5 | 7 | 8 | 2 | 2 | 7 | 8 | 2 | 5 | 7 | 8 | 2 | 1 | 5 | 2 | 7 | 8 | 2 | 1 | 1 | 2 | 7 | 8 | 2 |");
-// }
-
-void combinationAnalysis() { // Creating a "combinationAnalysis()" function to show a loading message
+void combinationAnalysis() {
     sleep(1);
 
     printf("\n Analisando combinação...");
@@ -47,7 +42,7 @@ void combinationAnalysis() { // Creating a "combinationAnalysis()" function to s
     sleep(1);
 }
 
-void successfulOpening() { // Creating a "successfulOpening()" function to show a success message when the safe is open
+void successfulOpening() {
     printf("\n Muito bem! Parece que você me conhece bem e conseguiu abrir o cofre com sucesso...");
     printf("\n A combinação que você descobriu/encontrou ao final dos deslizamentos é a chave correta para me desbloquear!");
 
@@ -57,7 +52,7 @@ void successfulOpening() { // Creating a "successfulOpening()" function to show 
     printf("\n\n Obrigado por utilizar meu serviço!\n");
 }
 
-void unsuccessfulOpening() { // Creating a "unccessfulOpening()" function to show a unsuccess message when the safe keep closed
+void unsuccessfulOpening() {
     printf("\n Que pena! Suas tentativas de abertura do cofre se esgotaram :(");
     printf("\n Parece que você não conseguiu encontrar a combinação correta para me abrir...");
 
@@ -66,16 +61,14 @@ void unsuccessfulOpening() { // Creating a "unccessfulOpening()" function to sho
     printf("\n\n Agradeço suas tentativas e lamento por qualquer incoveniente causado. Tenha um ótimo dia.\n");
 }
 
-int main() { // Creating the "main" structure of the code/project
-    setlocale(LC_ALL, "portuguese"); // Setting the project language to "portuguese" with the <locale.h> library
+int main() {
+    setlocale(LC_ALL, "portuguese");
 
-    int safe_bar[length] = {1, 5, 7, 8, 2, 2, 7, 8, 2, 5, 7, 8, 2, 1, 5, 2, 7, 8, 2, 1, 1, 2, 7, 8, 2}; // Creating the vector ("safe_bar") and including its values (1, 2, 5, 7, 8)
-    int trys = 0, k, i, movements, return_option, closure_option, initial_position, position_to; // Creating new variables to be used during the development of the code/project
-    int C1, C2, C5, C7, C8; // Creating five variables that are going to be used as counters
-    // int initial_position = 10, position_to = 0, password;
-    // int movements; // Incluir uma forma de perguntar ao usuário quantos movimentos/deslocamentos ele deseja realizar (no máximo 4)
+    int safe_bar[length] = {1, 5, 7, 8, 2, 2, 7, 8, 2, 5, 7, 8, 2, 1, 5, 2, 7, 8, 2, 1, 1, 2, 7, 8, 2};
+    int trys = 0, k, i, movements, return_option, closure_option, initial_position, position_to;
+    int C1, C2, C5, C7, C8;
 
-    introMessage(); // Calling function "introMessage()" to show the introduction message in the beginning
+    introMessage();
 
     sleep(1);
     system("pause");
@@ -117,28 +110,8 @@ int main() { // Creating the "main" structure of the code/project
 
         sleep(1);
 
-        // safeBarShowcase();
-
         printf("\n Tudo pronto! Agora, com a informação do número de deslizamentos para o processo de abertura, podemos prosseguir.");
         printf("\n Nessa etapa, será necessário informar as posições desejadas para o deslocamento do limite.\n");
-
-        /*
-        for(i = 0; i < movements; i++) {
-            printf("\n Informe o limite para o \"%dº\" deslocamento: ", i+1);
-            scanf("%d", &position_to);
-
-            if(position_to < 0) {
-                printf("\n O valor do limite informado foi negativo e, por esse motivo, a abertura do cofre foi finalizada.\n");
-                exit(0);
-            }
-        }
-
-        printf("\n Posição inicial: %d", initial_position);
-        printf("\n Posição limite: %d\n\n", position_to);
-
-        printf("\n Senha teste: ");
-        scanf("%d", &password);
-        */
 
         int flag = 1;
 
@@ -148,12 +121,6 @@ int main() { // Creating the "main" structure of the code/project
 
             if(position_to < 0) {
                 printf("\n Um número negativo foi informado e, consequentemente, o deslocamento foi encerrado!\n");
-
-                // sleep(1);
-
-                // exit(0);
-
-                // TODO: Adicionar um "switch" para possibilitar que o usuário retorne ao menu principal ou encerre o processo de abertura ao informar um número negativo.
 
                 sleep(3);
 
@@ -223,7 +190,6 @@ int main() { // Creating the "main" structure of the code/project
                         C8++;
                     }
                 }
-                // printf("\nC1: %d, C2: %d, C5: %d, C7: %d, C8: %d", C1, C2, C5, C7, C8);
                 initial_position = position_to;
             } else {
                 for (i = initial_position - 1; i >= position_to; i--) {
@@ -239,7 +205,6 @@ int main() { // Creating the "main" structure of the code/project
                         C8++;
                     }
                 }
-                // printf("\nC1: %d, C2: %d, C5: %d, C7: %d, C8: %d", C1, C2, C5, C7, C8);
                 initial_position = position_to;
             }
         }
@@ -252,21 +217,12 @@ int main() { // Creating the "main" structure of the code/project
             sleep(3);
             system("cls");
 
-            // C1 = 5 | C2 = 11 | C5 = 3 | C7 = 7 | C8 = 6
-
-            // if(password == 512376) {
             if((C1 == 5) && (C2 == 11) && (C5 == 3) && (C7 == 7) && (C8 == 6)) {
                 combinationAnalysis();
 
                 system("cls");
 
                 successfulOpening();
-
-                // sleep(2);
-                // system("pause");
-
-                // sleep(1);
-                // exit(0);
 
                 printf("\n --------------------------------------/--------------------------------------\n\n");
                 sleep(1);
@@ -353,6 +309,5 @@ int main() { // Creating the "main" structure of the code/project
 
     sleep(1);
 
-    // system("pause");
     return 0;
 }
